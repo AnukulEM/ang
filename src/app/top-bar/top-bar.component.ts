76dfdee6b405165403933d1e7948a22fcdd0f11d
AnukulEm <eme14917.gm@gmail.com>
2022-07-09 17:21:59 +0700

    17:21

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-top-bar',
+    template: './top-bar.componant.html',
+    styleUrls: ['./top-bar.componant.css']
+})
+
+export class TopBarComponant implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {
+        
+    }
+}
